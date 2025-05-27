79e23a18e00143d688f73d649220df2a4c04d30d
justputoff <kholid.hasibuan75@gmail.com>
2025-05-28 02:10:15 +0700

    c
    c

--- database/seeders/DistrictSeeder.php
+++ database/seeders/DistrictSeeder.php
@@ -16,49 +16,58 @@ class DistrictSeeder extends Seeder
     {
         $districts = [
             [
-                'name' => 'Bungus Teluk Kabung',
-                'sub_districts' => ['Bungus Barat', 'Bungus Selatan', 'Bungus Timur', 'Teluk Kabung Selatan', 'Teluk Kabung Tengah', 'Teluk Kabung Utara']
+                'name' => 'Cipondoh',
+                'sub_districts' => ['Cipondoh Makmur', 'Cipondoh Indah', 'Poris Plawad', 'Poris Plawad Indah']
             ],
             [
-                'name' => 'Koto Tangah',
-                'sub_districts' => ['Balai Gadang', 'Batang Kabung Ganting', 'Bungo Pasang', 'Dadok Tunggul Hitam', 'Koto Panjang Ikua Koto', 'Lubuk Buaya', 'Padang Sarai', 'Parupuk Tabing']
+                'name' => 'Karawaci',
+                'sub_districts' => ['Karawaci Baru', 'Karawaci Lama', 'Nusa Jaya', 'Cimone']
             ],
             [
-                'name' => 'Kuranji',
-                'sub_districts' => ['Anduring', 'Gunung Sarik', 'Kalumbuk', 'Korong Gadang', 'Kuranji', 'Lubuk Lintah', 'Pasar Ambacang', 'Sungai Sapih', 'Ampang']
+                'name' => 'Ciledug',
+                'sub_districts' => ['Ciledug Indah', 'Paninggilan', 'Sudimara Barat', 'Sudimara Timur']
             ],
             [
-                'name' => 'Lubuk Begalung',
-                'sub_districts' => ['Batuang Taba Nan XX', 'Batu Gadang', 'Cengkeh Nan XX', 'Gurun Laweh Nan XX', 'Kampung Baru Nan XX', 'Koto Baru Nan XX', 'Lubuk Begalung Nan XX', 'Pampangan Nan XX', 'Parak Laweh Pulau Aie Nan XX', 'Pegambiran Ampalu Nan XX', 'Pisang Nan XX', 'Tanah Sirah Piai Nan XX', 'Tanjung Saba Pitameh Nan XX', 'Tanjung Aur Nan XX', 'Gates Nan XX']
+                'name' => 'Batuceper',
+                'sub_districts' => ['Batuceper', 'Poris Gaga', 'Poris Gaga Baru']
             ],
             [
-                'name' => 'Lubuk Kilangan',
-                'sub_districts' => ['Bandar Buat', 'Batu Gadang', 'Beringin', 'Indarung', 'Koto Lalang', 'Padang Besi', 'Tarantang']
+                'name' => 'Benda',
+                'sub_districts' => ['Benda', 'Belendung', 'Jurumudi']
             ],
             [
-                'name' => 'Nanggalo',
-                'sub_districts' => ['Gurun Laweh', 'Kampung Lapai', 'Kampung Olo', 'Kurao Pagang', 'Surau Gadang', 'Tabing Banda Gadang']
+                'name' => 'Cibodas',
+                'sub_districts' => ['Cibodas Baru', 'Cibodas Sari', 'Jatiuwung', 'Panunggangan Barat']
             ],
             [
-                'name' => 'Padang Barat',
-                'sub_districts' => ['Belakang Tangsi', 'Kampung Jao', 'Kampung Pondok', 'Olo', 'Padang Pasir', 'Purus', 'Rimbo Kaluang']
+                'name' => 'Jatiuwung',
+                'sub_districts' => ['Jatake', 'Keroncong', 'Manis Jaya', 'Pasir Jaya']
             ],
             [
-                'name' => 'Padang Selatan',
-                'sub_districts' => ['Air Manis', 'Alang Laweh', 'Batang Arau', 'Belakang Olo', 'Bukit Gado-gado', 'Mato Aie', 'Pasa Gadang', 'Ranah Parak Rumbio', 'Rawang', 'Seberang Padang', 'Seberang Palinggam', 'Teluk Bayur']
+                'name' => 'Periuk',
+                'sub_districts' => ['Gebang Raya', 'Gembor', 'Periuk', 'Periuk Jaya']
             ],
             [
-                'name' => 'Padang Timur',
-                'sub_districts' => ['Andalas', 'Ganting Parak Gadang', 'Jati', 'Kubu Marapalam', 'Parak Gadang Timur', 'Sawahan', 'Sawahan Timur', 'Simpang Haru']
+                'name' => 'Pinang',
+                'sub_districts' => ['Kunciran', 'Kunciran Indah', 'Nerogtog', 'Pinang']
             ],
             [
-                'name' => 'Padang Utara',
-                'sub_districts' => ['Air Tawar Barat', 'Air Tawar Timur', 'Alai Parak Kopi', 'Gunung Pangilun', 'Lolong Belanti', 'Ulak Karang Selatan', 'Ulak Karang Utara']
+                'name' => 'Tangerang',
+                'sub_districts' => ['Babakan', 'Cikokol', 'Kelapa Indah', 'Sukaasih', 'Sukasari', 'Tanah Tinggi']
             ],
             [
-                'name' => 'Pauh',
-                'sub_districts' => ['Binuang Kampung Dalam', 'Cupak Tangah', 'Kapalo Koto', 'Koto Luar', 'Lambung Bukit', 'Limau Manis', 'Limau Manis Selatan', 'Pisang', 'Piai Tangah']
+                'name' => 'Karang Tengah',
+                'sub_districts' => ['Karang Mulya', 'Karang Tengah', 'Karang Timur', 'Parung Jaya']
             ],
+            [
+                'name' => 'Neglasari',
+                'sub_districts' => ['Karangsari', 'Kedaung Baru', 'Kedaung Wetan', 'Mekarsari', 'Neglasari']
+            ],
+            [
+                'name' => 'Larangan',
+                'sub_districts' => ['Cipadu', 'Cipadu Jaya', 'Gaga', 'Larangan Indah', 'Larangan Selatan', 'Larangan Utara']
+            ],
+            // Tambahkan distrik dan sub-distrik lainnya sesuai kebutuhan
         ];
 
         foreach ($districts as $districtData) {
